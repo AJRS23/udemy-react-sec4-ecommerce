b46fd31a406653fb5917c31907fe026b66f4bded
Alvaro Ramirez <alvaro.ramirez.seg@gmail.com>
2022-02-14 15:44:59 -0600

    sec5-80 React Router v6 added and small example

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Router>
+        <Routes> {/* This is the equivalent to Switch, in React Router v6 */}
+          <Route exact path="/" element={<HomePage/>} />
+          <Route path="/hats" element={<HatsPage/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
