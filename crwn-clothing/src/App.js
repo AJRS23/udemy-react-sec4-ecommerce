b58b939b29307afbbdd1d7bf7e7e397307a7bb30
Alvaro Ramirez <alvaro.ramirez.seg@gmail.com>
2022-02-16 13:23:43 -0600

    sec6-85 Shop page with collection preview and item components

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -2,23 +2,16 @@ import React from 'react';
 import { Route, Switch } from "react-router-dom";
 
 import './App.css';
-import HomePage from './pages/homepage/homepage.component.jsx';
 
-const HatsPage = (props) => {
-  console.log(props); //There are no props in React Router v6
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  )
-}
+import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
         <Switch> {/* This is the equivalent to Routes in React Router v6 */}
           <Route exact path="/" component={HomePage} />
-          <Route path="/hats" component={HatsPage}/>
+          <Route path="/shop" component={ShopPage}/>
         </Switch>
      
     </div>
