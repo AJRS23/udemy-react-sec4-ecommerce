cd13c4b4969d1dbe0ad28c9d446a24881b968bb8
Alvaro Ramirez <alvaro.ramirez.seg@gmail.com>
2022-03-03 10:49:07 -0600

    sec7-106 Firebase Google Sign in Auth get User logged

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -7,19 +7,45 @@ import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+import {auth} from './firebase/firebase.util';
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Switch> {/* This is the equivalent to Routes in React Router v6 */}
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage}/>
-        <Route path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-     
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user});
+
+      console.log(user);
+    })
+  }
+
+  //Avoid memory leak
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Switch> {/* This is the equivalent to Routes in React Router v6 */}
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage}/>
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      
+      </div>
+    );
+  }
 }
 
 
